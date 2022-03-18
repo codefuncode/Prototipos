ac2af0a3a8c1addb38692e3585c46ec93dfb2d7f
Carlos Aleman <caleman9791@gmail.com>
2022-03-18 10:10:06 -0400

    ok

---
+++ Proyectos/yees/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js">
+    </script>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link 1
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link 2
+          </a>
+        </li>
+      </ul>
+      <!-- Navbar text-->
+      <span class="navbar-text">
+        Navbar text
+      </span>
+    </nav>
+    <div class="container-fluid">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam suscipit quam eligendi adipisci deserunt illum delectus labore, saepe tempora animi et vel sed velit in, illo fugiat, culpa sequi!	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam suscipit quam eligendi adipisci deserunt illum delectus labore, saepe tempora animi et vel sed velit in, illo fugiat, culpa sequi!	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam suscipit quam eligendi adipisci deserunt illum delectus labore, saepe tempora animi et vel sed velit in, illo fugiat, culpa sequi!	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam suscipit quam eligendi adipisci deserunt illum delectus labore, saepe tempora animi et vel sed velit in, illo fugiat, culpa sequi!	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam suscipit quam eligendi adipisci deserunt illum delectus labore, saepe tempora animi et vel sed velit in, illo fugiat, culpa sequi!
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3">
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Inbox
+              <span class="badge badge-primary badge-pill">
+                12
+              </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Ads
+              <span class="badge badge-primary badge-pill">
+                50
+              </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Junk
+              <span class="badge badge-primary badge-pill">
+                99
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          col-6
+        </div>
+        <div class="col">
+          col
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <h2>
+        Large Modal
+      </h2>
+      <!-- Button to Open the Modal -->
+      <button class="btn btn-primary" data-target="#myModal" data-toggle="modal" type="button">
+        Open modal
+      </button>
+      <!-- The Modal -->
+      <div class="modal fade" id="myModal">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <h4 class="modal-title">
+                Modal Heading
+              </h4>
+              <button class="close" data-dismiss="modal" type="button">
+                ×
+              </button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+              Modal body..
+            </div>
+            <!-- Modal footer -->
+            <div class="modal-footer">
+              <button class="btn btn-secondary" data-dismiss="modal" type="button">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
